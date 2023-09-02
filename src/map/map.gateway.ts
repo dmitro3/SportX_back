f40a6fbec722d49a75942e46b55c8ced82a50e35
YaroDido <jarik.dido@gmail.com>
2023-09-02 12:59:30 +0200

    update: map gateway

---
+++ src/map/map.gateway.ts
@@ -0,0 +1,28 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class MapGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  afterInit(server: Server) {
+    console.log('WebSocket server initialized');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('Client connected');
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected');
+  }
+}
